8b7c513c08ef602ca4997e9e1e6cf76bd0de2d8a
sss-samurai <121731878+sss-samurai@users.noreply.github.com>
2025-02-18 16:15:38 +0530

    rust

---
+++ src/controllers/signup_user.rs
@@ -0,0 +1,34 @@
+use crate::models::user::User;
+use crate::utils::create_data::create_data;
+use actix_web::{web, HttpResponse, Responder};
+use serde::Serialize;
+use serde_json::{json, Value};
+#[derive(Debug, Serialize)]
+struct ErrorResponse {
+    error: String,
+}
+
+pub async fn signup_user(json_data: web::Json<Value>) -> impl Responder {
+    match serde_json::from_value::<User>(json_data.into_inner()) {
+        Ok(user) => {
+            if let Err(validation_error) = user.validate() {
+                return HttpResponse::BadRequest().json(ErrorResponse {
+                    error: validation_error,
+                });
+            }
+            match create_data("users", json!(user)).await {
+                Ok(created_user) => HttpResponse::Ok().json(json!({
+                    "message": "User created successfully",
+                    "data": created_user
+                })),
+                Err(e) => HttpResponse::InternalServerError().json(json!({
+                    "message": "Failed to create user",
+                    "error": e.to_string()
+                })),
+            }
+        }
+        Err(e) => HttpResponse::BadRequest().json(ErrorResponse {
+            error: format!("Invalid user data provided. Error: {}", e),
+        }),
+    }
+}
