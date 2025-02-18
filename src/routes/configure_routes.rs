38135bca9a97c27e64dbfd740176333c5d66fee6
sss-samurai <121731878+sss-samurai@users.noreply.github.com>
2025-02-18 16:18:24 +0530

    rust

--- src/routes/configure_routes.rs
+++ src/routes/configure_routes.rs
@@ -1,11 +1,15 @@
-// src/routes/configure_routes.rs
-use crate::controllers::create_product::create_product;
+use crate::controllers::create_and_edit_product::create_and_edit_product;
+use crate::controllers::delete_product::delete_product; // Corrected import
 use crate::controllers::first_function::first_function;
 use crate::controllers::get_product_data_by_id::get_product_data_by_id;
+use crate::controllers::signup_user::signup_user;
 use actix_web::web;
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-    cfg.route("/products", web::get().to(first_function)); // Use the function here
+    cfg.route("/products", web::get().to(first_function));
     cfg.route("/product/{id}", web::get().to(get_product_data_by_id));
-    cfg.route("/product", web::post().to(create_product));
+    cfg.route("/product", web::post().to(create_and_edit_product));
+    cfg.route("/login", web::post().to(create_and_edit_product));
+    cfg.route("/signup", web::post().to(signup_user));
+    cfg.route("/product/{id}", web::delete().to(delete_product));
 }
